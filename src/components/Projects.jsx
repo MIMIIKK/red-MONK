39c40c7cc587e0a46d6ad7f4a2c47fb52ac6a556
MIMIIK <injan9949@gmail.com>
2025-04-18 14:42:06 +0545

    changes final

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,7 @@ function Projects() {
       description: "AI-Solutions offers cutting-edge virtual assistant and prototyping solutions powered by artificial intelligence to enhance digital employee experiences. The product detail page showcases comprehensive specifications, customer testimonials, and includes a Schedule Demo form for interested customers to request personalized demonstrations.",
       image: "images/ai-solutions.png",
       tags: ["React", "Django", "SQLite"],
-      liveLink: "https://AI-Solutions-APP.vercel.app",
+      liveLink: "https://AI-Solutions-APP1.vercel.app",
       githubLink: "https://github.com/MIMIIKK/AI-Solutions.git"
     },
     {
